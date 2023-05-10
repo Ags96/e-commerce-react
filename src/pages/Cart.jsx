9e65ff7b7a63783bddb6f69aaec1767a600ccc96
Agustín Seoane <agustinseoane96@gmail.com>
2023-05-10 12:33:40 -0300

    first commit

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,27 @@
+import React, {useEffect} from "react"
+import {useDispatch, useSelector} from "react-redux"
+import {getAllProductsCartThunk} from "../store/slices/cart.slice"
+import ProductInCart from "../components/Cart/ProductInCart"
+
+const Cart = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getAllProductsCartThunk())
+  }, [])
+
+  const {cartGlobal} = useSelector(state => state)
+
+  console.log(cartGlobal)
+
+  return (
+    <div>
+      <h2>Cart</h2>
+      {cartGlobal?.map(prodCart => (
+        <ProductInCart key={prodCart.id} prodCart={prodCart} />
+      ))}
+    </div>
+  )
+}
+
+export default Cart
---
+++ src/pages/ProductId.jsx
@@ -0,0 +1,30 @@
+import React, {useEffect} from "react"
+import {useParams} from "react-router-dom"
+import useFetch from "../hooks/useFetch"
+import ProductIdInfo from "../components/ProductId/ProductIdInfo"
+import SliderImgs from "../components/ProductId/SliderImgs"
+import SimilarProducts from "../components/ProductId/SimilarProducts"
+import './styles/productId.css'
+
+const ProductId = () => {
+  const {id} = useParams()
+
+  const url = `https://e-commerce-api-v2.academlo.tech/api/v1/products/${id}`
+  const [product, getProductById] = useFetch(url)
+
+  useEffect(() => {
+    getProductById()
+  }, [])
+
+  return (
+    <div>
+      <div className="slider__info-container">
+        <SliderImgs product={product} />
+        <ProductIdInfo product={product} />
+      </div>
+        <SimilarProducts product={product} />
+    </div>
+  )
+}
+
+export default ProductId
