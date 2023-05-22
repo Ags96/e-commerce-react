6144c6c639827e7b31e05cf2ee2ba87688a1bec7
marcelYepes <marcelyepesqf@gmail.com>
2023-05-22 13:52:41 -0500

    fix border input

--- src/components/Purchases/ProductPurchase.jsx
+++ src/components/Purchases/ProductPurchase.jsx
@@ -1,10 +1,18 @@
+import "./styles/purchase.css"
+
 const ProductPurchase = ({prodPurchase}) => {
   return (
-    <article>
-      <img src={prodPurchase.product.images[0].url} alt="" />
-      <h3>{prodPurchase.product.title}</h3>
-      <span>{prodPurchase.quantity}</span>
-      <span>{prodPurchase.quantity * prodPurchase.product.price}</span>
+    <article className="purchases">
+      <img
+        className="purchases__img"
+        src={prodPurchase.product.images[0].url}
+        alt=""
+      />
+      <h3 className="purchases__title">{prodPurchase.product.title}</h3>
+      <span className="purchases__quantity">{prodPurchase.quantity}</span>
+      <span className="purchases__price">
+        {prodPurchase.quantity * prodPurchase.product.price}
+      </span>
     </article>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,6 @@ import FilterCategory from "../components/FilterCat/FilterCategory"
 import FilterByPrice from "../components/Home/FilterByPrice"
 import Footer from "../components/shared/Footer"
 
-
 const Home = () => {
   const [inputValue, setInputValue] = useState("")
   const [fromTo, setFromTo] = useState({
@@ -44,20 +43,31 @@ const Home = () => {
 
   return (
     <div className="home__body">
+      <div className="input__search-container">
+        <input
+          className="input__search-products"
+          ref={input}
+          onChange={handleChangeInput}
+          type="text"
+          placeholder="    what are you looking for?"
+        />
+        <button className="search__input-btn">
+          <i className="bx bx-search btn__search-p"></i>
+        </button>
+      </div>
       <div className="home__filter-category">
-        <div className="input__search-container">
-        <input className="input__search-products" ref={input} onChange={handleChangeInput} type="text" placeholder="what are you looking for?" />
-        <button className="search__input-btn"><i className='bx bx-search btn__search-p'></i></button>
-        </div>
         <section className="filter__section show__filter__section">
-            <FilterCategory />
-            <FilterByPrice setFromTo={setFromTo} />
-          </section>
-          <div className="home__products">
-            {productFilter?.map(prod => (
-              <CardProduct key={prod.id} product={prod} />
-            ))}
-          </div>
+          <FilterCategory />
+          <FilterByPrice setFromTo={setFromTo} />
+        </section>
+        <div className="home__products">
+          {productFilter?.map(prod => (
+            <CardProduct key={prod.id} product={prod} />
+          ))}
+        </div>
+        {/* <div className="footer">
+          <Footer />
+        </div> */}
       </div>
     </div>
   )
